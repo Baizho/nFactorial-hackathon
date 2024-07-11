96855e3419a5f4420a9b6301c8fb0bcbe9be0780
Nurmyrza Baizhanov <104897073+Baizho@users.noreply.github.com>
2024-07-11 15:36:48 +0500

    upd

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -66,5 +66,5 @@ readSpreadsheet();
 
 
 app.listen(process.env.PORT, () => {
-  console.log("server running at http://localhost:3000");
+  console.log("server running at http://localhost:5000");
 });
