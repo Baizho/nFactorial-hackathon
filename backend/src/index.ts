5eb2fef12a6ed18ba91e24a20c61c160223ceeb3
Nurmyrza Baizhanov <104897073+Baizho@users.noreply.github.com>
2024-07-12 13:10:13 +0500

    upd

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -13,7 +13,7 @@ const { google } = require('googleapis');
 const app = express();
 
 const corsOptions = {
-  origin: 'http://localhost:3000',
+  origin: 'https://n-factorial-hackathon-front.vercel.app',
   credentials: true,            //access-control-allow-credentials:true
   optionSuccessStatus: 200
 }
